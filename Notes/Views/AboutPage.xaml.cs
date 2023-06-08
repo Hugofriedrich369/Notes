9b50e6ed59456953f6a4441c204ddbd248b2881d
Hugofriedrich369 <hungos0223@gmail.com>
2023-06-07 22:37:02 -0300

    [Notes] Projeto Atualizado com MVVM

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -6,12 +6,4 @@ public partial class AboutPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.About about)
-        {
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
-    }
 }
--- Notes/Views/AllNotesPage.xaml.cs
+++ Notes/Views/AllNotesPage.xaml.cs
@@ -6,28 +6,5 @@ public partial class AllNotesPage : ContentPage
     {
         InitializeComponent();
 
-        BindingContext = new Models.AllNotes();
-    }
-
-    protected override void OnAppearing()
-    {
-        ((Models.AllNotes)BindingContext).LoadNotes();
-    }
-
-    private async void Add_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(NotePage));
-    }
-
-    private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (e.CurrentSelection.Count != 0)
-        {
-            var note = (Models.Note)e.CurrentSelection[0];
-
-            await Shell.Current.GoToAsync($"{nameof(NotePage)}?{nameof(NotePage.ItemId)}={note.Filename}");
-
-            notesCollection.SelectedItem = null;
-        }
     }
 }
